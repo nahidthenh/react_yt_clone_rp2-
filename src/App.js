1d26502be88c9e8c17b213d059ab1aa21e7a3b92
nahidthenh <nhgroup18@gmail.com>
2023-03-07 22:07:52 +0600

    React Router , mui and more package setup and setup react router with components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box } from '@mui/system';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navbar, ChannelDetail, Feed, SearchFeed, VideoDetail } from './Components';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <BrowserRouter>
+        <Box sx={{ backgroundColor: '#000' }}>
+          <Navbar />
+          <Routes>
+            <Route path='/' exact element={<Feed />} />
+            <Route path='/video/:id' element={<VideoDetail />} />
+            <Route path='/channel/:id' element={<ChannelDetail />} />
+            <Route path='/search/:searchTerm' element={<SearchFeed />} />
+          </Routes>
+        </Box>
+      </BrowserRouter >
+    </div >
   );
-}
+};
 
 export default App;
