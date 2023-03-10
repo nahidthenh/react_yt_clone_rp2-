19ab787f3cbd239fb17b4acb35cb69eb4b0e13bf
nahidthenh <nhgroup18@gmail.com>
2023-03-10 09:15:05 +0600

    FeedPage Working

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { Stack, Box, Typography } from '@mui/material'
+import { borderRight } from '@mui/system'
 
 function Feed() {
   return (
-    <div>Feed</div>
+    <Stack sx={{
+      flexDirection: { sx: 'column', md: 'row' }
+    }}>
+      <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
+
+      </Box>
+    </Stack >
   )
 }
 
